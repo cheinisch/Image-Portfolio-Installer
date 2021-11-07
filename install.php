bf0cc602bd7018cf8e2474c9c52d68180921f168
Christian Heinisch <chris@christian-heinisch.de>
2021-11-07 12:03:30 +0100

    Update install.php

---
+++ install.php
@@ -0,0 +1,8 @@
+<?php
+
+    // Installer für Image Portfolio
+    // Läd die aktuelle Version aus dem Netz runter und entpackt diese
+    // Es muss nur noch der Adminuser, DB Login, und Sitename angegeben werden
+    // Danach löscht sich der installer selbst
+
+?>
